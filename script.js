bc8354f92623829696d628d470f945844341f834
SiimonL <siimon.lellep@gmail.com>
2022-12-02 04:25:42 +0200

    Added extension and fixed stuff.

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 
 const display = document.querySelector('#display');
 
+
 const currOperation = {
     x: '0',
     y: '',
     operator: ''
 }
 
+
+function truncateNumber(number) {
+    let newNum = (number.startsWith('-')) ? number.slice(0, 11) : number.slice(0, 10);
+    
+    if (newNum !== number) {
+        if (newNum.at(-1) === '.') {
+            newNum = newNum.slice(0, -1);
+        } else if (!newNum.includes('.')) {
+            newNum = 'overflow';
+        }
+    }
+
+    return newNum;
+}
+
 function updateDisplay(value) {
     display.textContent = value;
 }
 
-function operate(operation) {
+function operateBinary(operation) {
     const x = parseFloat(operation.x);
     const y = parseFloat(operation.y);
     
@@ -23,15 +39,72 @@ function operate(operation) {
         case '*':
             return x * y;
         case '/':
+            if (y == 0) {
+                return 'E';
+            }
             return x / y;
+        case 'x^y':
+            return Math.pow(x, y);
+        case 'mod':
+            return x % y;
         default:
-            break;
+            return 'E';
+    }
+}
+
+function operateUrnary(operator, number) {
+    const x = parseFloat(number);
+    
+    switch (operator) {
+        case '+/-':
+            return (number.startsWith('-')) ? number.slice(1) : `-${number}`;
+        case 'sin':
+            return Math.sin(x);
+        case 'cos':
+            return Math.cos(x);
+        case 'tan':
+            return Math.tan(x);
+        case '2^x':
+            return Math.pow(2, x);
+        case 'sqrt':
+            if (x < 0) {
+                return 'E';
+            }
+            return Math.sqrt(x);
+        case 'ln':
+            if (x <= 0) {
+                return 'E';
+            }
+            return Math.log(x);
+        case 'log':
+            if (x <= 0) {
+                return 'E';
+            }
+            return Math.log10(x);
+        case '1/x':
+            if (x == 0)  {
+                return 'E';
+            }
+            return 1/x;
+        case '10^x':
+            return Math.pow(10, x);
+        default:
+            return 'E';
     }
 }
 
 // Digit buttons
 document.querySelectorAll('#number').forEach(btn => {
     btn.addEventListener('click', e => {
+        e.target.classList.add('clicked');
+        setTimeout(() => {
+            e.target.classList.remove('clicked');
+        }, 500);
+        
+        if (currOperation.x === 'E') {
+            currOperation.x = '';
+        }
+
         let digit;
         if (currOperation.operator === '') {
             digit = 'x';
@@ -46,9 +119,12 @@ document.querySelectorAll('#number').forEach(btn => {
             }
 
             currOperation[digit] += e.target.value;
+            currOperation[digit] = truncateNumber(currOperation[digit]);
         }
 
+        document.querySelectorAll('#binary').forEach(btn => {btn.classList.remove('clicked')});
         updateDisplay(currOperation[digit]);
+
     });  
 });
 
@@ -57,18 +133,47 @@ document.querySelectorAll('#binary').forEach(btn => {
     btn.addEventListener('click', e => {
         if (currOperation.operator === '' || currOperation.y === '') {
             document.querySelectorAll('#binary').forEach(button => button.classList.remove('selected'));
-            btn.classList.add('selected');
+            btn.classList.add('clicked');
 
             currOperation.operator = e.target.value;
-            updateDisplay('');
         }
+    });
+});
+
+// urnary operation buttons
+document.querySelectorAll('#urnary').forEach(btn => {
+    btn.addEventListener('click', e => {
+        let number;
+        if (currOperation.x !== '' && currOperation.operator === '') {
+            number = 'x';
+        } else if (currOperation.y !== '') {
+            number = 'y';
+        } else {
+            return new Error('umm idk');
+        }
+        
+        e.target.classList.add('clicked');
+        setTimeout(() => {
+            e.target.classList.remove('clicked');
+        }, 500);
+        
+        const result = truncateNumber(operateUrnary(e.target.value, currOperation[number]).toString());
+        updateDisplay(result);
+        currOperation[number] = result;
+        
     });
 });
 
 // equals button
 document.querySelector('#equals').addEventListener('click', e => {
     if (currOperation.y !== '') {
-        const result = operate(currOperation);
+        e.target.classList.add('clicked');
+        setTimeout(() => {
+            e.target.classList.remove('clicked');
+        }, 500);
+
+        let result = truncateNumber(operateBinary(currOperation).toString());
+        result = result == result ? result : 'Error';
         updateDisplay(result);
         currOperation.x = result;
         currOperation.operator = '';
@@ -78,17 +183,23 @@ document.querySelector('#equals').addEventListener('click', e => {
 
 // clear button
 document.querySelector('#clear').addEventListener('click', e => {
+    e.target.classList.add('clicked');
+    setTimeout(() => {
+        e.target.classList.remove('clicked');
+    }, 500);
+
     updateDisplay('0');
     currOperation.x = '';
     currOperation.operator = '';
     currOperation.y = '';
+    document.querySelectorAll('button').forEach(btn => {btn.classList.remove('clicked')});
 });
 
-// back button
-document.querySelector('#back').addEventListener('click', e => {
-    if (currOperation.operator === '') {
-        currOperation.x = currOperation.x.slice(0, -1);
-    } else if (currOperation.operator === '') {
-        currOperation.x = currOperation.x.slice(0, -1);
-    }
+
+// Extension toggle tab
+document.querySelector('#toggle').addEventListener('click', e => {
+    document.querySelector('#extension').classList.toggle('shown');
+    document.querySelector('#calculator').classList.toggle('extended');
+    
+    e.target.textContent = e.target.textContent === '+' ? '-' : '+';
 });
